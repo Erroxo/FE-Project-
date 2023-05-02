c21fffecaf8d7e3c1184cc4b58efbf1b2a45541e
Erroxo <123449096+Erroxo@users.noreply.github.com>
2023-05-02 21:11:45 +0530

    Add files via upload

---
+++ HTML CSS PROJECT/script.js
@@ -0,0 +1,13 @@
+const items = document.getElementsByClassName("button_z")
+for (let i of items) {
+  i.addEventListener("click", function() {
+      window.location.href = "../drawing tool js/Drawing tool.html";
+    });
+for (let i of items) {i.addEventListener("click", function() {
+  window.location.href = "../drawing tool js/Drawing tool.html";
+});
+
+}
+}
+
+
